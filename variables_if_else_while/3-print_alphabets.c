b1ef515997c87d14df0a3298cc501c7d6472f440
Juand983 <jdjimenezr98@gmail.com>
2022-10-13 14:50:39 -0700

    version 16

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+*
+*main - three
+*/
+
+int main(void)
+{
+char l;
+char al;
+for (l = 'a'; l <= 'z' ; l++)
+{
+putchar(l);
+}
+for (al = 'A' ; al <= 'Z' ; al++)
+{
+putchar(al);
+}
+putchar('\n');
+return (0);
+}
