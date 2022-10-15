6ba9234f0941f668f0f8ce9f1a7f9f0d17c4666a
Juand983 <jdjimenezr98@gmail.com>
2022-10-15 12:28:39 -0700

    version 19

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* main - three
-*@c: variable
-*Return: Always 0
-*/
-int _islower(int c);
+ * _islower - three
+ *@c: variable
+ *Return: Always 0
+ */
+int _islower(int c)
 {
-if (c >= 97 && c <= 122)
-return (1);
-else
-return (0);
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
 }
