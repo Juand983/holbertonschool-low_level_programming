e60a0a065f6f73316dea3a63a5521f1017792ac0
Juand983 <jdjimenezr98@gmail.com>
2022-10-15 12:19:50 -0700

    version 18.9

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
-*main - two
-*
-*Return: 0
-*/
+ *print_alphabet_x10 - two
+ *
+ *Return: Always 0
+ */
+
 void print_alphabet_x10(void)
 {
-int a, l;
-for (a = 0 ; a <= 10 ; a++)
-{
-for (l = 'a' ; l <= 'z' ; l++)
-_putchar(n);
-_putchar('\n');
-}
+	int a, l;
+
+	for (a = 0 ; a <= 10 ; a++)
+	{
+		for (l = 'a' ; l <= 'z' ; l++)
+			_putchar(l);
+		_putchar('\n');
+	}
 }
