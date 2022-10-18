6d1a94822ba5a3188a79e681e98c50d0b3732515
Juand983 <jdjimenezr98@gmail.com>
2022-10-18 11:10:03 -0700

    version 21.3

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *swap_int - twos integers
+ *@a: aporte
+ *@b: aporte 2
+ *
+ *Return: entero
+ */
+
+void swap_int(int *a, int *b)
+{
+int n;
+	n = *a;
+	*a = *b;
+	*b = n;
+}
