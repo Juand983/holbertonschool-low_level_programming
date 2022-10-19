8876b82447bc68d44e1e847ca42ea87a84747267
Juand983 <jdjimenezr98@gmail.com>
2022-10-19 14:07:35 -0700

    version 23

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen - cambio
+ *@char *s: caracter
+ *@s: nuevo
+ *Return: _strlen
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
