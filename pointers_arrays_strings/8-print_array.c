b5f27cd9b368f0e989a67d90f7e8b95cd4d61501
Juand983 <jdjimenezr98@gmail.com>
2022-10-23 04:24:32 -0700

    version 27

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_array - vector int
+ *@*a: array
+ *@n: numbers
+ *@a: a
+ *
+ */
+void print_array(int *a, int n)
+{
+int i;
+for(i = 0 ; i < n ; i++)
+	{
+	printf("%d", a[i]);
+	if (i != (n -1))
+		{
+		printf(",");
+		printf(" ");
+		}
+	}
+printf('\n');
+}
