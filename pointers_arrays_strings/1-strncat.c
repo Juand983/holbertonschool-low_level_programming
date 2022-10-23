ac5d37b2b76f4d98e17b9c9d30c954e687ad8fb1
Juand983 <jdjimenezr98@gmail.com>
2022-10-23 09:28:07 -0700

    version 27.4

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strncat - concatenar
+ *@src: origen
+ *@dest: destino
+ *@n: bytes
+ *Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, k;
+
+	for(i = 0 ; dest[i] != '\0'; i++)
+	{
+	}
+	for (k = 0 ; k < n ; k++)
+	{
+		dest[i + k] = src[k];
+		if(src[k] == '\0')
+			k = n;
+	}
+	return (dest);
+}
