d24a878d20da2d120312b447c56c7098617c6686
Juand983 <jdjimenezr98@gmail.com>
2022-10-23 18:25:28 -0700

    version 27.4

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,13 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, k;
 
-	for(i = 0 ; dest[i] != '\0'; i++)
+	for (i = 0 ; dest[i] != '\0'; i++)
 	{
 	}
 	for (k = 0 ; k < n ; k++)
 	{
 		dest[i + k] = src[k];
-		if(src[k] == '\0')
+		if (src[k] == '\0')
 			k = n;
 	}
 	return (dest);
