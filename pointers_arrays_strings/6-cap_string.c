e253a4c8165ed8453632da04fa2c73d7fd67a22c
Juand983 <jdjimenezr98@gmail.com>
2022-10-25 07:34:10 -0700

    version 27.9

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *cap_string - string changes
+ *@a: string
+ *Return: a
+ */
+char *cap_string(char *a)
+{
+int i, j;
+char sep[] = " \t\n,;.!?\"(){}";
+i = 1;
+if (a[0] >= 'a' && a[0] <= 'z')
+	a[0] -= ('a' - 'A');
+while (a[i] != '\0')
+{
+for (j = 0; sep[j] != '\0'; j++)
+	if (a[i - 1] == sep[j] && (a[i] >= 'a' && a[i] <= 'z'))
+		a[i] -= ('a' - 'A');
+i++;
+}
+return (a);
+}
